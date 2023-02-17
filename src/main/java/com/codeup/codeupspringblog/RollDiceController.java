069da435fa46da8c159a759de53c2d2d01384d19
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-02-17 15:46:08 -0600

    completed dice roller portion of views exercise

--- src/main/java/com/codeup/codeupspringblog/RollDiceController.java
+++ src/main/java/com/codeup/codeupspringblog/RollDiceController.java
@@ -1,14 +1,31 @@
 package com.codeup.codeupspringblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 public class RollDiceController {
 
     @GetMapping("/roll-dice")
     public String rollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDiceGuess(@PathVariable int guess, Model model){
+        model.addAttribute("guess", guess);
+        int answer = new Random().nextInt(6) + 1;
+        model.addAttribute("answer", answer);
+        if (guess == answer){
+            model.addAttribute("correctGuess", true);
+        } else {
+            model.addAttribute("incorrectGuess", true);
+        }
         return "roll-dice";
     }
 
