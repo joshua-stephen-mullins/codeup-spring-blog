48a32acd2088f9137fd136ecfa90c74e2e4eb032
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-02-20 16:38:00 -0600

    finished form model binding exercise

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -10,7 +10,6 @@ public class PostController {
     private final PostRepository postDao;
     private final UserRepository userDao;
 
-
     public PostController(PostRepository postDao, UserRepository userDao) {
         this.postDao = postDao;
         this.userDao = userDao;
@@ -37,12 +36,11 @@ public class PostController {
         return "/posts/edit";
     }
 
-    @PostMapping("/posts/{id}/edit")
-    public String editPostById(@PathVariable long id, Model model, @ModelAttribute Post post) {
-        Post originalPost = postDao.getById(id);
-        model.addAttribute("post", post);
-        model.addAttribute("originalPost", originalPost);
-        return "/posts/edit";
+    @PostMapping("/posts/edit")
+    public String editPostById(@RequestParam(name="id") int id, @ModelAttribute Post post) {
+        post.setId(id);
+        postDao.save(post);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
